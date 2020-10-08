99667dae66efa256425ff9ca4cb3034bd829c825
tom liu <tom.liu@macys.com>
2020-10-08 11:17:34 -0700

    first commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,20 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { within } from "@testing-library/dom";
+import { App } from "./App";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("it works", () => {
+  const root = document.createElement("div");
+  ReactDOM.render(<App />, root);
+
+  const { getByText, getByLabelText } = within(root);
+
+  // expect(getByText("TODOS")).not.toBeNull();
+  // expect(getByLabelText("What needs to be done?")).not.toBeNull();
+  // expect(getByText("Add #1")).not.toBeNull();
+
+  // Above code can be shortend to
+  getByText("TODOS");
+  getByLabelText("What needs to be done?");
+  getByText("Add #1");
 });
